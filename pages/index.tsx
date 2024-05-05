efeca67e1903dd2789fb50b81a6ab88cebdc9107
User <user@macbook-pro.lan>
2024-05-05 03:45:39 -0400

    base route redirect to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useDisclosure} from "@mantine/hooks";
-
+import {useRouter} from "next/router";
 
 export default function IndexPage() {
 
     const [opened, { toggle }] = useDisclosure();
+    const router = useRouter()
+
+    useEffect(() => {
+        router.push('/home')
+    }, []);
 
     return (
-      <>
-          <h1>index</h1>
-      </>
+      <></>
   );
 }
