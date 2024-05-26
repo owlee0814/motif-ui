92390a8c3fba3ed230bdcceddb658aedbd86cf20
User <user@macbook-pro.lan>
2024-05-26 04:05:58 -0400

    update cache control to route side

--- app/api/posts/route.tsx
+++ app/api/posts/route.tsx
@@ -13,18 +13,10 @@ export async function GET(req: Request) {
             },
         });
 
-        const headers = new Headers({
-            "Content-Type": "application/json",
-            "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate"
-        });
-
         if (!posts) {
             return new Response("Post not found", { status: 404 });
         }
-        return new Response(JSON.stringify(posts), {
-            status: 200,
-            headers: headers
-        });
+        return new Response(JSON.stringify(posts), { status: 200 });
     } catch (error) {
         return new Response("Internal Server Error", { status: 500 });
     }
--- pages/community/c/[community]/index.tsx
+++ pages/community/c/[community]/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     useEffect(() => {
         const fetchGet = async () => {
             try {
-                const response = await fetch('/api/posts');
+                const response = await fetch('/api/posts', { cache: 'no-store' });
                 if (!response.ok) {
                     throw new Error('Failed to fetch post');
                 }
