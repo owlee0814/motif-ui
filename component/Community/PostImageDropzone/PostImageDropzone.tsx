848754a736d3db329710c2c130a35d9cfe6a55ba
User <user@macbook-pro.lan>
2024-05-31 07:57:19 -0400

    limit files upload to 5

--- component/Community/PostImageDropzone/PostImageDropzone.tsx
+++ component/Community/PostImageDropzone/PostImageDropzone.tsx
@@ -1,9 +1,9 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
-import { ActionIcon, AspectRatio, Center, Group, Image, Overlay, Title } from '@mantine/core';
+import { ActionIcon, AspectRatio, Center, Group, Image, Overlay, Title, Text } from '@mantine/core';
 import { IconPhoto, IconTrash, IconUpload, IconX } from '@tabler/icons-react';
 import classes from "./PostImageDropzone.module.css";
-import {Carousel, CarouselSlide} from "@mantine/carousel";
+import { Carousel } from "@mantine/carousel";
 
 type FileWithPreview = {
     file: File;
@@ -17,24 +17,31 @@ interface PostImageDropZoneProps {
 export default function PostImageDropZone({ onFileSelected }: PostImageDropZoneProps) {
     const [files, setFiles] = useState<FileWithPreview[]>([]);
     const [dropzoneHeight, setDropzoneHeight] = useState(200);
-    const [dropzoneWidth, setDropzoneWidth] = useState('100%');
-    const imageRef = useRef<HTMLImageElement>(null);
+    const [error, setError] = useState<string | null>(null);
     const dropzoneRef = useRef<HTMLDivElement>(null);
 
     const handleDrop = (acceptedFiles: File[]) => {
+        if (files.length + acceptedFiles.length > 5) {
+            console.log('asdf')
+            setError('You can only upload up to 5 images.');
+            return;
+        }
+
         const newFiles = acceptedFiles.map((file) => ({
             file,
             preview: URL.createObjectURL(file),
         }));
-        setDropzoneHeight(500)
-        files.length == 0 ? setFiles(newFiles) : setFiles(files => [...files, ...newFiles]);
+        setDropzoneHeight(500);
+        files.length === 0 ? setFiles(newFiles) : setFiles(files => [...files, ...newFiles]);
         onFileSelected(acceptedFiles[0]); // Pass the first selected file to the parent
+        setError(null);
     };
 
     const handleRemove = (index: number) => {
+        setError(null);
         setFiles(files.filter((_, i) => i !== index));
-        files.length == 1 ? setDropzoneHeight(200) : setDropzoneHeight(500)
-        onFileSelected(null); // Pass null to the parent when the file is removed
+        files.length === 1 ? setDropzoneHeight(200) : setDropzoneHeight(500);
+        onFileSelected(null);
     };
 
     const previews = files.map((file, index) => (
@@ -44,7 +51,7 @@ export default function PostImageDropZone({ onFileSelected }: PostImageDropZoneP
                     <Image
                         src={file.preview}
                         alt={`preview ${index}`}
-                        width={dropzoneWidth}
+                        width={dropzoneHeight}
                         height={dropzoneHeight}
                     />
                 </AspectRatio>
@@ -53,7 +60,7 @@ export default function PostImageDropZone({ onFileSelected }: PostImageDropZoneP
                         <Image
                             src={file.preview}
                             alt={`preview ${index}`}
-                            style={{ width: dropzoneWidth, height: '500px', maxHeight: '500px' }}
+                            style={{ width: '100%', height: '500px', maxHeight: '500px' }}
                         />
                         <ActionIcon
                             variant="filled"
@@ -61,8 +68,8 @@ export default function PostImageDropZone({ onFileSelected }: PostImageDropZoneP
                             radius="xl"
                             size="lg"
                             onClick={(event) => {
-                                event.stopPropagation()
-                                handleRemove(index)
+                                event.stopPropagation();
+                                handleRemove(index);
                             }}
                             className={classes.actionIcon}
                         >
@@ -75,42 +82,45 @@ export default function PostImageDropZone({ onFileSelected }: PostImageDropZoneP
     ));
 
     return (
-        <div style={{ width: '100%', height: dropzoneHeight }}>
-            <Dropzone
-                ref={dropzoneRef}
-                onDrop={handleDrop}
-                onReject={(files) => console.log('rejected files', files)}
-                accept={IMAGE_MIME_TYPE}
-                className={classes.dropzone}
-            >
-                <div className={classes.previewsContainer}>
-                    <Carousel
-                        draggable={false}
-                        withControls={files.length > 1}
-                        withIndicators={files.length > 1}
-                        height={'500px'}
-                        onClick={(event) => {
-                            // @ts-ignore
-                            if(event.target.localName !== 'div')
-                                event.stopPropagation()
-                        }}
-                    >
-                        {previews}
-                    </Carousel>
-                </div>
-                <Group className={classes.group}>
-                    <Dropzone.Accept>
-                    <IconUpload size={30} stroke={1.5} />
-                    </Dropzone.Accept>
-                    <Dropzone.Reject>
-                        <IconX size={30} stroke={1.5} />
-                    </Dropzone.Reject>
-                    <Dropzone.Idle>
-                        <IconPhoto size={30} stroke={1.5} />
-                    </Dropzone.Idle>
-                    <Title size="1.25rem">Drag images (up to 5) here or click to select files</Title>
-                </Group>
-            </Dropzone>
-        </div>
+        <>
+            <div style={{ width: '100%', height: dropzoneHeight }}>
+                <Dropzone
+                    ref={dropzoneRef}
+                    onDrop={handleDrop}
+                    onReject={(files) => console.log('rejected files', files)}
+                    accept={IMAGE_MIME_TYPE}
+                    className={classes.dropzone}
+                >
+                    <div className={classes.previewsContainer}>
+                        <Carousel
+                            draggable={false}
+                            withControls={files.length > 1}
+                            withIndicators={files.length > 1}
+                            height={'500px'}
+                            onClick={(event) => {
+                                // @ts-ignore
+                                if(event.target.localName !== 'div')
+                                    event.stopPropagation()
+                            }}
+                        >
+                            {previews}
+                        </Carousel>
+                    </div>
+                    <Group className={classes.group}>
+                        <Dropzone.Accept>
+                            <IconUpload size={30} stroke={1.5} />
+                        </Dropzone.Accept>
+                        <Dropzone.Reject>
+                            <IconX size={30} stroke={1.5} />
+                        </Dropzone.Reject>
+                        <Dropzone.Idle>
+                            <IconPhoto size={30} stroke={1.5} />
+                        </Dropzone.Idle>
+                        <Title size="1.25rem">Drag images here or click to select files (up to 5) </Title>
+                    </Group>
+                </Dropzone>
+            </div>
+            {error && <Text c="red" mt={'xs'}>{error}</Text>}
+        </>
     );
 }
